0160c40be33bc71f2bc16a103a3fb0d5757e345b
DiegoLeandro477 <diegoleandro477@gmail.com>
2023-05-27 19:42:24 -0300

    Creating entity Post with nested User

--- src/main/java/com/ferruje/workshopmongo/config/Instantiation.java
+++ src/main/java/com/ferruje/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.ferruje.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.ferruje.workshopmongo.domain.Post;
 import com.ferruje.workshopmongo.domain.User;
+import com.ferruje.workshopmongo.repository.PostRepository;
 import com.ferruje.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,16 +19,28 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRep;
 	
+	@Autowired
+	private PostRepository postRep;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRep.deleteAll();
+		postRep.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2023"), "Partiu viagem", "Vou viajar para São Paulo. Abreços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2023"), "Bom dia", "Acordei feliz hoje!", bob);
+
 
 		userRep.saveAll(Arrays.asList(maria, alex, bob));
+		postRep.saveAll(Arrays.asList(post1, post2));
 		
 	}
 
---
+++ src/main/java/com/ferruje/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.ferruje.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ferruje.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+}
