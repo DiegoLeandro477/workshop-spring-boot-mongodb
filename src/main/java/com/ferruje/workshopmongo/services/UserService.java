090f6a1d3be4ee128ea2e7074aff8e8ac9cf3727
DiegoLeandro477 <diegoleandro477@gmail.com>
2023-05-26 23:26:38 -0300

    user update with PUT

--- src/main/java/com/ferruje/workshopmongo/services/UserService.java
+++ src/main/java/com/ferruje/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.ferruje.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,18 +24,29 @@ public class UserService {
 	public User findById(String id) {
 		return rep.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return rep.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		rep.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return rep.save(newObj);
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
 	}
 
 }
